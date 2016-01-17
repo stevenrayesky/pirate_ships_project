71cce99cf37e4a1c9ad02ce38ebeb5b750e4cda7
Emily McCracken <emilymcc803@gmail.com>
2016-01-17 13:37:24 -0500

    paperclip working for boats.. can upload and display

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -3,4 +3,10 @@ class Boat < ActiveRecord::Base
 	has_many :users, through: :expeditions
 
 	validates :name, uniqueness: {message: "this boat has already been created"}
+
+	has_attached_file :image, :styles =>
+	{ :medium => "300x300>", :thumb => "100x100>" },
+	:default_url => "/images/:style/missing.png"
+	validates_attachment_content_type :image,
+	:content_type => /\Aimage\/.*\Z/
 end
