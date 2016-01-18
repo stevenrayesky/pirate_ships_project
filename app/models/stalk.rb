b980678872caef6f58bee6800c62e34f2937a866
Steven Rayesky <Protolexus@gmail.com>
2016-01-17 20:32:39 -0500

    Closed #1. A user can now stalk a boat. Created the stalks controller that handles creating stalks.

--- app/models/stalk.rb
+++ app/models/stalk.rb
@@ -1,2 +1,4 @@
 class Stalk < ActiveRecord::Base
+	belongs_to :user
+	belongs_to :boat
 end
---
+++ test/controllers/stalks_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class StalksControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
