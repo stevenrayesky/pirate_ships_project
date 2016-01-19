b5335ca6eb8bd5709335a488c8e9e13e85c4aed8
Steven Rayesky <Protolexus@gmail.com>
2016-01-19 09:51:31 -0500

    Fixed problem with boats loading into proper island divs. Removed unused helper from mapshelper.

--- app/helpers/maps_helper.rb
+++ app/helpers/maps_helper.rb
@@ -9,20 +9,4 @@ module MapsHelper
 			image_tag 'pirate_ship.png', class: 'boat_image'
 		end
 	end
-	# expedition_form will be hidden on the maps#index until a user clicks on a boat.
-	def expedition_form(boat, current_user)
-		form_for @expedition, html: {class: "expedtion_form"} do |f|
-			f.label :Name
-			f.text_field :name
-			f.label :Captains_Log
-			f.text_field :captains_log
-			f.label :Stolen_From
-			f.select :origin, ['Port Royal', 'Tortuga', 'Barataria Bay', 'Canary Islands']
-			f.label :Taken_To
-			f.select :destination, ['Port Royal', 'Tortuga', 'Barataria Bay', 'Canary Islands']
-			f.hidden_field(:user_id, :value => @current_user.id)
-			f.hidden_field(:boat_id, :value => boat.id)
-			f.submit :create
-		end
-	end
 end
